7eb4fca9951a344f8c32c3fce1b1e62f960d44a9
87crosby <71791452+87crosby@users.noreply.github.com>
2024-06-25 12:42:18 -0400

    Add test scenarios to project

--- calculate_reimbursement.py
+++ calculate_reimbursement.py
@@ -2,6 +2,24 @@ from datetime import datetime, timedelta
 
 class Project:
     def __init__(self, city_type, start_date, end_date):
-        self.city_type = city_type
+        self.city_type = city_type # 'high' or 'low' cost city
         self.start_date = datetime.strptime(start_date, "%m/%d/%y")
         self.end_date = datetime.strptime(end_date, "%m/%d/%y")
+
+
+
+# Define the scenarios
+set1 = [Project("low", "9/1/15", "9/3/15")]
+
+set2 = [Project("low", "9/1/15", "9/1/15"),
+        Project("high", "9/2/15", "9/6/15"),
+        Project("low", "9/6/15", "9/8/15")]
+
+set3 = [Project("low", "9/1/15", "9/3/15"),
+        Project("high", "9/5/15", "9/7/15"),
+        Project("high", "9/8/15", "9/8/15")]
+
+set4 = [Project("low", "9/1/15", "9/1/15"),
+        Project("low", "9/1/15", "9/1/15"),
+        Project("high", "9/2/15", "9/2/15"),
+        Project("high", "9/2/15", "9/3/15")]
